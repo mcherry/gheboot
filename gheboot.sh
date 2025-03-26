a45e7211feef15307d99db1a55846276045764af
Mike Cherry <mcherry@mcgithub.local>
2025-03-26 08:42:58 -0500

    Added error checking and documented defaults

--- gheboot.sh
+++ gheboot.sh
@@ -1,18 +1,18 @@
-#!/bin/bash
+#!/bin/bash -e
 #/ GitHub Enterprise Server Boot
 #/ Usage: gheboot [-vhbscmnd]
 #/
 #/ Creates and boots a new GitHub Enterprise Server appliance in Proxmox.
 #/
 #/ OPTIONS:
-#/   -v        | --version       Version of GHES to install
-#/   -h        | --hostname      Hostname for new virtual machine.
-#/   -b        | --bootstorage   Name of boot disk storage.
-#/   -s        | --ssdstorage    Name of SSD storage.
-#/   -c        | --cores         Number of vCPU cores.
-#/   -m        | --memory        Amount of RAM in megabytes.
-#/   -n        | --network       Network device name.
-#/   -d        | --download      Download QCOW2 image for specified version.
+#/   -v  |  --version      Version of GHES to install
+#/   -h  |  --hostname     Hostname for new virtual machine.
+#/   -b  |  --bootstorage  Name of boot disk storage.
+#/   -s  |  --ssdstorage   Name of SSD storage.
+#/   -c  |  --cores        Number of vCPU cores. Default: 8
+#/   -m  |  --memory       Amount of RAM in megabytes. Default: 65535
+#/   -n  |  --network      Network device name. Default: vmbr0
+#/   -d  |  --download     Download QCOW2 image for specified version.
 #/
 #/ EXAMPLE:
 #/ 
@@ -27,6 +27,7 @@ CORES="8"
 MEMORY="65535"
 NETWORK="vmbr0"
 
+function error() { echo "ERROR: $1"; exit 1; }
 function show_help() { grep '^#/' < "$0" | cut -c4-; exit 1; }
 if [ "$1" == "" ]; then show_help; fi
 
@@ -80,23 +81,27 @@ VM_ID=`pvesh get /cluster/nextid`
 GHES_URL="https://github-enterprise.s3.amazonaws.com/kvm/releases/github-enterprise-$GHES_VERSION.qcow2"
 
 if [ "$GHES_DOWNLOAD" == "1" ]; then
-  wget "$GHES_URL"
+  wget "$GHES_URL" || error "Failed to download $GHES_URL"
 fi
 
-qm create $VM_ID \
-  --name "$GHES_HOSTNAME" \
-  --net0 virtio,bridge=$NETWORK \
-  --ostype l26 \
-  --memory $MEMORY \
-  --onboot no \
-  --cpu cputype=host \
-  --sockets 1 \
-  --cores $CORES \
-  --vga qxl
+if [ -f "github-enterprise-$GHES_VERSION.qcow2" ]; then
+  qm create $VM_ID \
+    --name "$GHES_HOSTNAME" \
+    --net0 virtio,bridge=$NETWORK \
+    --ostype l26 \
+    --memory $MEMORY \
+    --onboot no \
+    --cpu cputype=host \
+    --sockets 1 \
+    --cores $CORES \
+    --vga qxl || error "Failed to create VM $GHES_HOSTNAME ($VM_ID)"
 
-qm importdisk $VM_ID "github-enterprise-$GHES_VERSION.qcow2" "$BOOT_STORAGE"
-qm set $VM_ID --scsi0 $BOOT_STORAGE:vm-$VM_ID-disk-0
-qm set $VM_ID --boot order=scsi0
-pvesm alloc "$SSD_STORAGE" "$VM_ID" vm-$VM_ID-disk-1 200G
-qm set $VM_ID --scsihw virtio-scsi-pci --scsi1 $SSD_STORAGE:vm-$VM_ID-disk-1,discard=on,ssd=1
-qm start $VM_ID
+  qm importdisk $VM_ID "github-enterprise-$GHES_VERSION.qcow2" "$BOOT_STORAGE" || error "Failed to import root disk into appliance"
+  qm set $VM_ID --scsi0 $BOOT_STORAGE:vm-$VM_ID-disk-0 || error "Failed to configure boot disk"
+  qm set $VM_ID --boot order=scsi0 || error "Failed to set boot order"
+  pvesm alloc "$SSD_STORAGE" "$VM_ID" vm-$VM_ID-disk-1 200G || error "Failed to allocate data storage disk"
+  qm set $VM_ID --scsihw virtio-scsi-pci --scsi1 $SSD_STORAGE:vm-$VM_ID-disk-1,discard=on,ssd=1 || error "Failed to set data storage disk options"
+  qm start $VM_ID || error "Failed to boot appliance"
+else
+  error "Disk image 'github-enterprise-$GHES_VERSION.qcow2' not found"
+fi
