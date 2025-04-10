c5e576997900581be1bdfbf3212471b7132839d1
Mike Cherry <mcherry@inditech.org>
2025-04-10 11:47:04 -0500

    Updated help output

--- gheboot.sh
+++ gheboot.sh
@@ -23,12 +23,14 @@
 #/   -k  |  --insecure     Allow insecure HTTPS connection for initial configuration.
 #/
 #/ EXAMPLE:
-#/ 
+#/
 #/ # Download GHES version 3.15.4, import it into "local" storage as "ghes-primary",
 #/ # create a data drive on "local-ssd" storage, create a VM with 8 cores, 64GB ram,
-#/ # and use the configured "vmbr0" network bridge
-#/ 
-#/ gheboot.sh -v 3.15.4 -h ghes-primary -b local -s local-ssd -c 8 -m 65535 -n vmbr0 -p -d
+#/ # and use the configured "vmbr0" network bridge. Also configure root site password and
+#/ # upload license.
+#/
+#/ gheboot.sh -v 3.15.4 -h ghes-primary -b local -s local-ssd -c 8 -m 65535 -n vmbr0 \
+#/     -r "YourPassword123@" -l license-file.ghl -i 192.168.1.0/24 -k -p -d
 
 GHES_DOWNLOAD=0
 CORES="8"
