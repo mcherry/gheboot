63907a454383067f9aedbc436202cbb259509e29
Mike Cherry <mcherry@inditech.org>
2025-04-10 11:30:33 -0500

    Added options for setting root site password and license upload

--- gheboot.sh
+++ gheboot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 #/ GitHub Enterprise Server Boot
-#/ Usage: gheboot [-vhbscmnd]
+#/ Usage: gheboot [-vhbscmnrlpodk]
 #/
 #/ Creates and boots a new GitHub Enterprise Server appliance in Proxmox.
 #/
@@ -12,8 +12,15 @@
 #/   -c  |  --cores        Number of vCPU cores. Default: 8
 #/   -m  |  --memory       Amount of RAM in megabytes. Default: 65535
 #/   -n  |  --network      Network device name. Default: vmbr0
+#/   -r  |  --root         Root site password for initial configuration.
+#/   -l  |  --license      Path to license file.
+#/   -i  |  --ipnetmask    IP network that the VM will be on. This is necessary
+#/                         for the initial configuration to work and should be
+#/                         in CIDR format. Example: 192.168.1.0/24
+#/   -p  |  --poweron      Power on VM after creation.
 #/   -o  |  --onboot       Configure VM to boot when hypervisor reboots.
 #/   -d  |  --download     Download QCOW2 image for specified version.
+#/   -k  |  --insecure     Allow insecure HTTPS connection for initial configuration.
 #/
 #/ EXAMPLE:
 #/ 
@@ -21,13 +28,16 @@
 #/ # create a data drive on "local-ssd" storage, create a VM with 8 cores, 64GB ram,
 #/ # and use the configured "vmbr0" network bridge
 #/ 
-#/ gheboot.sh -v 3.15.4 -h ghes-primary -b local -s local-ssd -c 8 -m 65535 -n vmbr0 -d
+#/ gheboot.sh -v 3.15.4 -h ghes-primary -b local -s local-ssd -c 8 -m 65535 -n vmbr0 -p -d
 
 GHES_DOWNLOAD=0
 CORES="8"
 MEMORY="65535"
 NETWORK="vmbr0"
 ONBOOT="no"
+POWERON=0
+ALLOW_INSECURE=""
+IP_NETMASK=""
 
 function print_help() { grep '^#/' < "$0" | cut -c4-; exit 1; }
 function print_error() { echo "ERROR: $1"; exit 1; }
@@ -53,7 +63,100 @@ function create_vm() {
   run_command "qm" "set ${ghes_vm[id]} --boot order=scsi0" || error "Failed to configure boot order"
   run_command "pvesm" "alloc '${ghes_vm[ssd_storage]}' '${ghes_vm[id]}' vm-${ghes_vm[id]}-disk-1 200G" || print_error "Failed to allocate storage disk"
   run_command "qm" "set ${ghes_vm[id]} --scsihw virtio-scsi-pci --scsi1 ${ghes_vm[ssd_storage]}:vm-${ghes_vm[id]}-disk-1,discard=on,ssd=1" || print_error "Failed to configure storage disk"
-  run_command "qm" "start ${ghes_vm[id]}" || print_error "Failed to boot"
+  
+  if [ "${ghes_vm[poweron]}" == "1" ]; then
+    run_command "qm" "start ${ghes_vm[id]}" || print_error "Failed to boot"
+  fi
+}
+
+# Function to configure the initial settings of the VM
+#
+# This includes setting the root password and uploading the license file
+#
+# It uses the VM ID, IP netmask, root password, license file path, and insecure flag
+# as parameters
+#
+# It finds the VM's IP address by pinging the subnet and then configures the VM
+# using the curl command to send a POST request to the VM's management endpoint
+#
+# The function retries the configuration process a few times in case of failure
+function initial_config() {
+  local vm_id=$1
+  local ip_netmask=$2
+  local site_password=$3
+  local license_file=$4
+  local insecure=$5
+  local hostname=$(run_command "hostname")
+
+  # Ping the subnet to hopefully get the VM's IP in the ARP table
+  echo "* Attempting to find VM IP address, this might take a few minutes..."
+  for ip in $(list_subnet_ips $ip_netmask); do
+    timeout 0.2 ping -i 0.2 -c1 "$ip" > /dev/null 2>&1
+  done
+
+  for i in {1..5}; do
+    echo -n "* Looking for VM IP address (attempt $i of 5)..."
+    
+    mac_address=$(run_command "pvesh" "get /nodes/$hostname/qemu/$vm_id/config --noborder|grep net0|cut -d ',' -f1|cut -d '=' -f2")
+    ip_address=$(run_command "ip" "neigh show|grep -i $mac_address|grep -v fe80|cut -d ' ' -f 1")
+
+    if [ "$ip_address" != "" ]; then
+      echo "found $ip_address"
+      for a in {1..5}; do
+        echo "* Configuring root site password and uploading license file (attempt $a of 5)..."
+        config_output=$(run_command "curl" "$insecure -s -L -X POST -u 'api_key:your-password' -H 'Content-Type: multipart/form-data' https://$ip_address/manage/v1/config/init --form 'license=@$license_file' --form 'password=$site_password' 2>&1")
+        if (echo "$config_output" | grep "Sorry"); then
+          [ $a -lt 5 ] && sleep 45
+        else
+          echo "Initial configuration completed. You can finish setting up your GHES instance at https://$ip_address:8443/setup"
+          break 
+        fi
+      done
+
+      break
+    fi
+
+    [ $i -lt 5 ] && sleep 5
+    echo
+  done
+  if [ "$ip_address" == "" ]; then
+    echo "* Failed to find VM IP address"
+  fi
+}
+
+# Function to convert CIDR to netmask
+cidr_to_netmask() {
+    local cidr=$1
+    local mask=""
+    for ((i=1; i<=32; i++)); do
+        if [ $i -le $cidr ]; then
+            mask="${mask}1"
+        else
+            mask="${mask}0"
+        fi
+    done
+
+    echo "$((2#${mask:0:8})).$((2#${mask:8:8})).$((2#${mask:16:8})).$((2#${mask:24:8}))"
+}
+
+# Function to generate all IP addresses in a subnet
+list_subnet_ips() {
+    local network=$(echo $1|cut -d '/' -f 1)
+    local cidr=$(echo $1|cut -d '/' -f 2)
+    local netmask=$(cidr_to_netmask "$cidr")
+    local IFS=.
+    read -r i1 i2 i3 i4 <<< "$network"
+    read -r m1 m2 m3 m4 <<< "$netmask"
+
+    for ((a=$i1; a<=$((i1|(255-m1))); a++)); do
+        for ((b=$i2; b<=$((i2|(255-m2))); b++)); do
+            for ((c=$i3; c<=$((i3|(255-m3))); c++)); do
+                for ((d=$i4; d<=$((i4|(255-m4))); d++)); do
+                    echo "$a.$b.$c.$d"
+                done
+            done
+        done
+    done
 }
 
 if [ "$1" == "" ]; then print_help; fi
@@ -86,9 +189,25 @@ while [[ $# -gt 0 ]]; do
       MEMORY="$2"
       shift 2
       ;;
-     -n|--network)
+    -n|--network)
       NETWORK="$2"
       shift 2
+      ;;
+    -r|--root)
+      ROOT_PASSWORD="$2"
+      shift 2
+      ;;
+    -l|--license)
+      LICENSE_FILE="$2"
+      shift 2
+      ;;
+    -i|--ipnetmask)
+      IP_NETMASK="$2"
+      shift 2
+      ;;
+    -p|--poweron)
+      POWERON=1
+      shift
       ;;
     -o|--onboot)
       ONBOOT="yes"
@@ -96,6 +215,10 @@ while [[ $# -gt 0 ]]; do
       ;;
     -d|--help)
       GHES_DOWNLOAD=1
+      shift
+      ;;
+    -k|--insecure)
+      ALLOW_INSECURE="-k"
       shift
       ;;
     -*|--*)
@@ -126,8 +249,15 @@ if [ -f "github-enterprise-$GHES_VERSION.qcow2" ]; then
     [memory]="$MEMORY"
     [network]="$NETWORK"
     [onboot]="$ONBOOT"
+    [poweron]="$POWERON"
   )
   create_vm new_vm
+
+  if [ -n "$ROOT_PASSWORD" ] && [ -n "$LICENSE_FILE" ]; then
+    echo "* Waiting for VM to boot..."
+    sleep 60
+    initial_config "$VM_ID" "$IP_NETMASK" "$ROOT_PASSWORD" "$LICENSE_FILE" "$ALLOW_INSECURE" || print_error "Initial configuration failed"
+  fi
 else
   print_error "Disk image 'github-enterprise-$GHES_VERSION.qcow2' not found"
 fi
